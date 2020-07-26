09db010e5c470050b13c04321695dac37c752b65
Tim Shaker <tim@tmshkr.com>
2020-07-26 14:08:05 -0700

    toggle cell

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,16 +6,32 @@ function Board(props) {
   const [rows, setRows] = useState(Math.floor(window.innerHeight / 20));
   const [cols, setCols] = useState(Math.floor(window.innerWidth / 20));
   const [matrix, setMatrix] = useState(create2DMatrix(rows, cols));
+  const [isRunning, setRunning] = useState(false);
   console.log("render");
+
+  const handleClick = (e) => {
+    if (isRunning) setRunning(false);
+    const { i, j } = e.target.dataset;
+    e.target.classList.toggle("alive");
+    matrix[i][j] = matrix[i][j] ? 0 : 1;
+    console.log(matrix);
+  };
 
   return (
     <div
       id="board"
       style={{ gridTemplateColumns: `repeat(${cols}, 1fr)` }}
-      onClick={(e) => console.log(e.target)}
+      onClick={handleClick}
     >
       {matrix.map((row, i) =>
-        row.map((cell, j) => <span key={`${i},${j}`} data-i={i} data-j={j} />)
+        row.map((cell, j) => (
+          <span
+            key={`${i},${j}`}
+            className={cell ? "alive" : ""}
+            data-i={i}
+            data-j={j}
+          />
+        ))
       )}
     </div>
   );
