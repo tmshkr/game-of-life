504f3c2a3ffc1a4eff13ef5f6682990dc6d49180
Tim Shaker <tim@tmshkr.com>
2020-07-29 19:42:02 -0700

    improve input handling

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,29 @@ function Header(props) {
   const handleInput = (e) => {
     const target = e.target;
     let num = Number(target.innerHTML);
-    if (isNaN(num)) num = 500;
+    if (isNaN(num)) num = interval;
     else if (num < 50) num = 50;
     else if (num > 1000) num = 1000;
-    if (timerRef.current) clearTimeout(timerRef.current);
+    clearTimeout(timerRef.current);
     timerRef.current = setTimeout(() => {
+      target.blur();
       if (num === interval) target.innerHTML = num;
       else app.setState({ interval: num });
     }, 1000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.which === 13) {
+      e.preventDefault();
+      e.target.blur();
+      clearTimeout(timerRef.current);
+      let num = Number(e.target.innerHTML);
+      if (isNaN(num)) num = interval;
+      else if (num < 50) num = 50;
+      else if (num > 1000) num = 1000;
+      if (num === interval) e.target.innerHTML = num;
+      else app.setState({ interval: num });
+    }
   };
 
   return (
@@ -29,6 +44,7 @@ function Header(props) {
         <p
           contentEditable
           onInput={handleInput}
+          onKeyDown={handleKeyDown}
           dangerouslySetInnerHTML={{ __html: interval }}
         />
         <p>ms</p>
